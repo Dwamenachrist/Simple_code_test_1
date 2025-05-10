a599a26724efa8f253ccd845057fed563c4cf53d
Dwamenachrist <agyapongfrancis520@gmail.com>
2025-05-10 10:50:37 +0000

    Simple Codes to test the AI Agent

---
+++ math_utils.py
@@ -0,0 +1,9 @@
+'''A simple math utility module.'''
+
+def is_even(n):
+  """Checks if a number is even."""
+  return n % 2 == 0
+
+def is_odd(n):
+  """Checks if a number is odd."""
+  return n % 2 != 0
---
+++ string_utils.py
@@ -0,0 +1,9 @@
+'''A simple string utility module.'''
+
+def reverse_string(s):
+  """Reverses a string."""
+  return s[::-1]
+
+def is_palindrome(s):
+  """Checks if a string is a palindrome."""
+  return s == s[::-1]
