a6b4995eb8af81b37422261ff87dca939df89a5b
Dwamenachrist <136202826+Dwamenachrist@users.noreply.github.com>
2025-05-12 22:38:35 +0000

    Delete math_utils.py

--- math_utils.py
+++
@@ -1,9 +0,0 @@
-'''A simple math utility module.'''
-
-def is_even(n):
-  """Checks if a number is even."""
-  return n % 2 == 0
-
-def is_odd(n):
-  """Checks if a number is odd."""
-  return n % 2 != 0
