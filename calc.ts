7e3aa4eeece1647e5de65fa61e3c7117aed01f7f
Dwamenachrist <136202826+Dwamenachrist@users.noreply.github.com>
2025-05-16 03:27:24 +0000

    Create calc.ts

---
+++ calc.ts
@@ -0,0 +1,10 @@
+// File: add.ts
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export class Calculator {
+  add(a: number, b: number): number {
+    return a + b;
+  }
+}
